582aceef37da237bbb27737878bb599041d0e5bb
chisonghongmei98 <149028930+chisonghongmei98@users.noreply.github.com>
2023-10-26 11:35:15 +0900

    Update valid.php

--- api/valid.php
+++ api/valid.php
@@ -1,6 +1,10 @@
 <?php
 define('ENVIRONMENT', 'production');
 
+header("Cache-Control: no-store, no-cache, max-age=0");
+// header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 include "../core/disposable.php";
 
 $validmail = new VerifyEmail();
